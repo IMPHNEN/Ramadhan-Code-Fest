0ae98a2b24334cdc9fd2fd11257a2affac39573c
Nabil Anugerah Pangestu <nabilpangestu123@gmail.com>
2024-03-12 16:59:11 +0700

    upload project todolist ramadhan
    project ini dibuat menggunakan react js dan media penyimpanannya menggunakan localstorage

---
+++ Todolist-Ramadhan/src/App.jsx
@@ -0,0 +1,13 @@
+// import './App.css'
+
+import FormTodoList from "./components/FormTodoList";
+
+function App() {
+  return (
+    <>
+      <FormTodoList />
+    </>
+  );
+}
+
+export default App;
